9c04a09023988f6a0668256e3c03a92c654f54b3
lizziesilver <lizzie.silver@silverpond.com.au>
2022-01-07 23:10:04 +1100

    readding mmcv, need it for img preprocess

--- colab-scripts/setup-infer-mmdet.sh
+++ colab-scripts/setup-infer-mmdet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# # Needed for mmcv
-# apt install -y libmagic-dev
+# Needed for mmcv
+apt install -y libmagic-dev
 
 # Pip install, order matters I think
 # at least install mmdet last
@@ -9,7 +9,7 @@ pip3 install torch==1.10.1+cu113 torchvision==0.11.2+cu113 -f https://download.p
 
 pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha==0.6
 
-# pip3 install mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html
+pip3 install mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html
 
 pip3 install onnx onnxruntime==1.8.1
 
