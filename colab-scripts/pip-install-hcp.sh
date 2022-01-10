75af791c67fba5c6aef1ebf7fb61fd4b7ad71b56
josh <joshua.patterson@silverpond.com.au>
2022-01-10 15:59:02 +1100

    update hcp version. Read hcp version from pyproject toml when pip installing in colab

--- colab-scripts/pip-install-hcp.sh
+++ colab-scripts/pip-install-hcp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+PYPROJECT=`"${BASH_SOURCE%/*}/pyproject.toml"`
+HCP_VERSION=`grep 'highlighter-client-v2-alpha' pyproject.toml | grep -o '[\"].*[\"]' | cut -d '"' -f2`
+
+echo "Installing highlighter-client-v2-alpha${HCP_VERSION}"
+
 # Needed for opencv
 apt install -y libmagic-dev
 
-pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha==0.6"
+pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha${HCP_VERSION}"
