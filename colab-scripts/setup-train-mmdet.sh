32a1ef83b5874cdf627c996f46cc70e0b3272ad2
josh <joshua.patterson@silverpond.com.au>
2022-01-10 11:58:30 +1100

    update to hcpv2-alpha 0.6

--- colab-scripts/setup-train-mmdet.sh
+++ colab-scripts/setup-train-mmdet.sh
@@ -7,7 +7,7 @@ apt install -y libmagic-dev
 # at least install mmdet last
 pip3 install torch==1.10.1+cu113 torchvision==0.11.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
-pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha==0.5
+pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha==0.6
 
 pip3 install mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html
 
