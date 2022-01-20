c294f50fdc54ef998b426a18cf6dae50833bd88a
Lizzie Silver <lizziesilver@gmail.com>
2022-01-20 14:31:43 +1100

    Remove trailing double quote typo

--- colab-scripts/pip-install-hcp.sh
+++ colab-scripts/pip-install-hcp.sh
@@ -8,4 +8,4 @@ echo "Installing highlighter-client-v2-alpha${HCP_VERSION}"
 # Needed for opencv
 apt install -y libmagic-dev
 
-pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha${HCP_VERSION}"
+pip3 install -i https://${PYPI_USERNAME}:${PYPI_PASSWORD}@pypi.silverpond.com.au/simple highlighter-client-v2-alpha${HCP_VERSION}
